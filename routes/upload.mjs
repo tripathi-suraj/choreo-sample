29fb43eafe78ec7a017397089bb2e504ddce010f
Suraj Tripathi <surajtripathi781@gmail.com>
2024-07-11 08:57:22 +0530

    Upload Files Success

--- routes/upload.mjs
+++ routes/upload.mjs
@@ -1,39 +1,26 @@
 import multer from "multer";
-import path from "path";
-import { dirname,join,extname } from 'path';
-import { fileURLToPath } from 'url';
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-const fileStorage = multer.diskStorage({
+import * as path from "path";
+const fileFilter = (req, file, cb) => {
+    if (['image/png', 'image/jpg', 'image/jpeg'].includes(file.mimetype)) {
+        cb (null, true)
+    } else {
+        cb (null, false)
+    }
+}
+const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null,'/uploads')
+      cb(null, 'uploads/'); // Specify the directory where files will be stored
     },
     filename: (req, file, cb) => {
-      cb(null, (new Date()).toISOString() + '-' +  file.originalname)
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)); // Generate a unique file name
     }
-})
-  
-const fileFilter = (req, file, cb) => {
-if (['image/png', 'image/jpg', 'image/jpeg'].includes(file.mimetype)) {
-    cb (null, true)
-} else {
-    cb (null, false)
-}
-}
+});
+const upload = multer({ storage:storage ,fileFilter:fileFilter})
 
 function attachRoutes(app){
-    console.log(process.base);
-    app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
-    app.post('/uploader',(req,res)=>{
+    app.post('/uploader',upload.single('image'),(req,res)=>{
         if(!req.file){
             return res.status(400).json({ message: "Not allowed only png jpg jpeg are allowed to upload" });        
-        }
-
-        const fileTypes = /jpeg|jpg|png/;
-        const extname = fileTypes.test(extname(req.file.originalname).toLowerCase());
-        const mimeType = fileTypes.test(req.file.mimetype);
-        if (!extname || !mimeType) {
-            return res.status(400).json({ message: "Not allowed only png jpg jpeg are allowed to upload" });
         }
         const { deviceId} = req.body;
         res.send('Uploaded');
