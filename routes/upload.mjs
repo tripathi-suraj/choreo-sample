a6525381f423bdcb2f692422364865d58540bff7
Suraj Tripathi <surajtripathi781@gmail.com>
2024-07-11 11:20:10 +0530

    API KEY

--- routes/upload.mjs
+++ routes/upload.mjs
@@ -15,15 +15,19 @@ const storage = multer.diskStorage({
       cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)); // Generate a unique file name
     }
 });
-const upload = multer({ storage:storage ,fileFilter:fileFilter})
-
+const upload = multer({ storage:storage ,fileFilter:fileFilter});
 function attachRoutes(app){
     app.post('/uploader',upload.single('image'),(req,res)=>{
+        if(req.headers.apikey !== "e1bd2282-f444-4893-b704-239d036110e4"){
+            return res.status(403).json({ message: "Provide Valid Apikey" });
+        }
+
         if(!req.file){
             return res.status(400).json({ message: "Not allowed only png jpg jpeg are allowed to upload" });        
         }
+
         const { deviceId} = req.body;
-        res.send('Uploaded');
+        res.send('File Uploaded Successfully');
     })
 }
 
